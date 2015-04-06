cfd1ef5d3a215d65258cdd0d87c768d2db63924e
dacaiguoguo <dacaiguoguo@163.com>
2015-04-06 15:33:04 +0800

    Initial Commit

---
+++ NSColorList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSColorList
+//
+//  Created by sunyanguo on 4/6/15.
+//  Copyright (c) 2015 sunyanguo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
